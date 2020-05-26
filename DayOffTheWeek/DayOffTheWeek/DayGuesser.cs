1117a05ce6097c0a09f7e6aa9fc2fb1673f2d49a
damian.1991.kaminski <damian.1991.kaminski@gmail.com>
2020-05-26 23:06:01 +0200

    .

--- DayOffTheWeek/DayOffTheWeek/DayGuesser.cs
+++ DayOffTheWeek/DayOffTheWeek/DayGuesser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using DayOffTheWeekApp.Core;
+using DayOffTheWeekApp.Core.Enums;
 
 namespace DayOffTheWeek
 {
@@ -9,27 +10,49 @@ namespace DayOffTheWeek
     {
         public DayCalculator Calculator { get; set; }
 
+        public DateTimeOffset UserDateOdBirth { get; set; }
+
+        public DayOfTheWeek UserDayOfTheWeek { get; set; }
+
         public void IntroduceTheApplication()
         {
             Console.WriteLine("Hey, potrafię wyliczyć dzień tygodnia na podstawie Twojej daty urodzenia. ");
+            Calculator = new DayCalculator();
         }
 
         public void AskUserForTheirDateOfBirth()
         {
-            Console.WriteLine("Podaj datę urodzenia");
-            var userDate = Console.ReadLine();
-            
+           Console.WriteLine("Podaj datę urodzenia");
+           var userDate = Console.ReadLine();
+
+           var succeded = DateTimeOffset.TryParse(userDate, out var date );
+
+            if (succeded)
+            {
+                UserDateOdBirth = date;
+                return;
+            }
+
+            Console.WriteLine("Błędny format daty. Prawidłowy to dd/mm/yyyy");
+            AskUserForTheirDateOfBirth();
+
 
         }
 
         public void CalculateDayOfTheWeek()
         {
+            if(UserDateOdBirth == null)
+            {
+                Console.WriteLine("starano się obliczyć dzień tygodnia bez podania daty urodzenia");
+            }
+
+            UserDayOfTheWeek = Calculator.CalculateDayOfTheWeek(UserDateOdBirth);
 
         }
 
         public void PrintDayOfTheWeek()
         {
-            Console.WriteLine("Obliczony dzień tygodnia to: "+ "test");
+            Console.WriteLine("Obliczony dzień tygodnia to: "+ UserDayOfTheWeek);
         }
 
     }
--- DayOffTheWeek/DayOffTheWeekApp.Core/DayCalculator.cs
+++ DayOffTheWeek/DayOffTheWeekApp.Core/DayCalculator.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using DayOffTheWeekApp.Core.Enums;
+using System;
 using System.Collections.Generic;
 
 namespace DayOffTheWeekApp.Core
 {
     public class DayCalculator
-    {
+    {        
+        public DayOfTheWeek CalculateDayOfTheWeek(DateTimeOffset data)
+        {
 
+            var day = data.Day;
+            var month = data.Month;
+            var year = data.Year;
+
+                var listOfParameters = new List<int> { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+                year -= month < 3? 1: 0;
+                var calculatedValue = (year + year / 4 - year / 100 + year / 400 + listOfParameters[month - 1] + day) % 7;
+            //Console.WriteLine(calculatedValue);
+            return (DayOfTheWeek)(calculatedValue - 1);
+        }
     }
 }
